df14882ac099950a33a36bc3f02788725d0b62d9
Loader Huang <loader.huang@dxc.com>
2022-09-21 11:44:20 +0800

    可自訂傳入參數

--- Demo01_01_nine.java
+++ Demo01_01_nine.java
@@ -1,23 +1,34 @@
+import java.util.Scanner;
+
 public class Demo01_01_nine {
     public static void main(String[] args) {
-//        tryVersion1(5); //階梯
-//        tryVersion2(5); //矩形
-//        tryVersion3(5); //偶數列的偶數格空白
-        tryVersion4(5); //反向階梯
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("請輸入列數:");
+        int rowNum = Integer.parseInt(userInput.nextLine());
 
+        tryVersion1(rowNum); //階梯
+        tryVersion2(rowNum); //矩形
+        tryVersion3(rowNum); //偶數列的偶數格空白
+        tryVersion4(rowNum); //反向階梯
     }
 
     private static void tryVersion4(int row) {
-        for (int i = 1; i<=row;i++){
-            for(int j =1;j<=row-i;j++){
+        System.out.println("=====Version4:偶數列的偶數格空白=====");
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= row - i; j++) {
                 System.out.print(" ");
             }
-            System.out.println("*");
+            for (int k = row - i; k < row; k++) {
+                System.out.print("*");
+            }
+            if (i < row) {  //避免印出多餘的空白行
+                System.out.println();
+            }
         }
-
     }
 
     private static void tryVersion3(int row) {
+        System.out.println("=====Version3:偶數列的偶數格空白=====");
         for (int i = 1; i <= row; i++) {
 
             for (int j = 1; j <= row; j++) {
@@ -29,27 +40,26 @@ public class Demo01_01_nine {
             }
             System.out.println();
         }
-        System.out.println("=====分隔線=====");
     }
 
     private static void tryVersion2(int row) {
+        System.out.println("=====Version2:矩形=====");
         for (int i = 1; i <= row; i++) {
             for (int j = 1; j <= row; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        System.out.println("=====分隔線=====");
     }
 
     private static void tryVersion1(int row) {
+        System.out.println("=====Version1：階梯=====");
         for (int i = 1; i <= row; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        System.out.println("=====分隔線=====");
     }
 }
-//ctrl+alt+l 自動排版
+//Windows Intellij 的自動排版快捷鍵：ctrl+alt+l
